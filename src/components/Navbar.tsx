ae3ea350b737b862981bdfe0ce63a2b53168c991
MUSTAFAREZA47 <mustafareza47@gmail.com>
2024-06-27 18:41:56 +0530

    Herosection-completed

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,64 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+
+const Navbar = ({ className }: { className?: string }) => {
+    const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
+        </Link>
+
+        <MenuItem setActive={setActive} active={active} item="Our Courses">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">Offline Courses</HoveredLink>
+            <HoveredLink href="/courses">Online Courses</HoveredLink>
+            <HoveredLink href="/courses">Recorded Courses</HoveredLink>
+            <HoveredLink href="/courses">1 on 1 Mentorship</HoveredLink>
+          </div>
+        </MenuItem>
+
+        <Link href={"/about"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="About Us"
+          ></MenuItem>
+        </Link>
+        <Link href={"/resources"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Resources"
+          ></MenuItem>
+        </Link>
+        <Link href={"/blogs"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Blogs"
+          ></MenuItem>
+        </Link>
+        <Link href={"/contact"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contact Us"
+          ></MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  );
+};
+
+export default Navbar;
